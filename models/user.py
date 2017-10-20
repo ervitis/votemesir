7536c1463409a0954f2b5b6de775f4b63dad8238
ervitis <vitomarti@gmail.com>
2017-10-20 22:35:49 +0200

    feat: model generate random data

--- models/user.py
+++ models/user.py
@@ -10,3 +10,39 @@ class ModelUser(ndb.Model):
     password = ndb.StringProperty()
     name = ndb.StringProperty()
     surname = ndb.StringProperty()
+
+
+def generate_data(n):
+    assert (isinstance(n, int) is True and 0 < n < 1001)
+    password_prefix = 'Ab_Cd_'
+
+    def generate_word():
+        import random
+        import string
+
+        vowels = 'aeiou'
+        consonants = ''.join(set(string.ascii_lowercase) - set(vowels))
+
+        w = ''
+        for i in range(random.choice(range(7, 12))):
+            if i % 2 == 0:
+                w += random.choice(vowels)
+            elif i % 5 == 0:
+                w += random.choice(consonants) * 2
+            else:
+                w += random.choice(consonants)
+        return w
+
+    ldata = []
+    for _ in range(n):
+        m = {
+            'email': generate_word() + '@votemesir.appspotmail.com',
+            'url_email_validation': '',
+            'date_last_vote': None,
+            'password': password_prefix + generate_word(),
+            'name': generate_word(),
+            'surname': generate_word()
+        }
+        ldata.append(m)
+
+    return ldata
--- tests/test_model.py
+++ tests/test_model.py
@@ -3,7 +3,7 @@
 import unittest
 
 from datetime import datetime
-from models.user import ModelUser
+from models.user import (ModelUser, generate_data)
 
 
 class TestModel(unittest.TestCase):
@@ -28,6 +28,38 @@ class TestModel(unittest.TestCase):
     def test_model_properties(self):
         for attr, value in self._test_parameters.items():
             self.assertEqual(getattr(self.model, attr), value)
+
+    def test_auto_generate_data(self):
+        models = generate_data(5)
+        self.assertEqual(len(models), 5)
+        self.assertTrue(any([model.get('email') for model in models]))
+        self.assertTrue(any([model.get('password') for model in models]))
+        self.assertTrue(any([model.get('name') for model in models]))
+        self.assertTrue(any([model.get('surname') for model in models]))
+
+    def test_auto_generate_data_url_and_date_are_empty(self):
+        models = generate_data(2)
+        self.assertFalse(any([model.get('url_email_validation') for model in models]))
+        self.assertFalse(any([model.get('date_last_vote') for model in models]))
+
+    def test_all_generated_data_are_not_equal(self):
+        models = generate_data(1000)
+        i = 0
+        for m in models[i:]:
+            j = i + 1
+            for n in models[j:]:
+                if n == m:
+                    self.fail('{n} and {m} are equal in iteration {i}, {j}'.format(n=n, m=m, i=i, j=j))
+                j += i
+            i += 1
+
+    def test_parameter_generate_data_int(self):
+        self.assertRaises(AssertionError, generate_data, '2')
+        self.assertRaises(AssertionError, generate_data, 4.5)
+
+    def test_parameter_generate_data_between_1_and_1000(self):
+        self.assertRaises(AssertionError, generate_data, 0)
+        self.assertRaises(AssertionError, generate_data, 1001)
 
 
 if __name__ == '__main__':
