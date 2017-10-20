d3ce2319dbcb590fa568869b5b277e4894d3a4b1
ervitis <vitomarti@gmail.com>
2017-10-20 19:15:42 +0200

    fix: debug appengine pythonpath

--- appengine/setup_gcloud.sh
+++ appengine/setup_gcloud.sh
@@ -72,7 +72,7 @@ function installAppengine {
 }
 
 function installAppengineGcloud {
-    sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-python
+    sudo /opt/google-cloud-sdk/bin/gcloud components install -q app-engine-python
 }
 
 function installSDK {
