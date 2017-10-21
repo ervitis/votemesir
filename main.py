dd1adf3d48813cc5c44e2b8aa2f238c9a1033097
ervitis <vitomarti@gmail.com>
2017-10-21 10:00:12 +0200

    feat: poc email

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from __future__ import print_function
 
 import logging
-from libs.flask import Flask
+from libs.flask import Flask, request
 
 
 def create_app(name):
@@ -19,14 +19,5 @@ def hello():
 
 
 @app.route('/_ah/mail', methods=['POST'])
-def receiver_handler_email(mail_message):
-    logging.info("Received a message from: " + mail_message.sender)
-    plaintext_bodies = mail_message.bodies('text/plain')
-    html_bodies = mail_message.bodies('text/html')
-
-    for content_type, body in html_bodies:
-        decoded_html = body.decode()
-        logging.info("Html body of length %d.", len(decoded_html))
-    for content_type, body in plaintext_bodies:
-        plaintext = body.decode()
-        logging.info("Plain text body of length %d.", len(plaintext))
+def receiver_handler_email():
+    logging.info("Received a message from: " + request.form)
