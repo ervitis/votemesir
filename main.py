81999bdf128f157b1b718a478e562aa58bac60d5
ervitis <vitomarti@gmail.com>
2017-10-21 09:23:14 +0200

    feat: email handler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+import logging
 from libs.flask import Flask
 
 
@@ -15,3 +16,17 @@ app = create_app(__name__)
 @app.route('/ping')
 def hello():
     return 'hello world'
+
+
+@app.route('/_ah/mail')
+def receiver_handler_email(mail_message):
+    logging.info("Received a message from: " + mail_message.sender)
+    plaintext_bodies = mail_message.bodies('text/plain')
+    html_bodies = mail_message.bodies('text/html')
+
+    for content_type, body in html_bodies:
+        decoded_html = body.decode()
+        logging.info("Html body of length %d.", len(decoded_html))
+    for content_type, body in plaintext_bodies:
+        plaintext = body.decode()
+        logging.info("Plain text body of length %d.", len(plaintext))
